8cf31df98fb97d426b0cffc62de5e42d78f46b8f
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2017-06-02 13:51:11 +0900

    🚧コンパスを作る：回転の原点をコンパスの中心にした

---
+++ Assets/Scripts/Gizmo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gizmo : MonoBehaviour
+{
+
+    public float gizmoSize = 0.3f;
+    public Color gizmoColor = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 5));
+    }
+}
