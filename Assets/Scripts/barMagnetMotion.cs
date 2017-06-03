905ef96957257697ea5f6179f372ef4e16735476
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2017-06-04 07:38:22 +0900

    🚧コンパスを作る：5 x 5 x 5の立体格子を作る

--- Assets/Scripts/barMagnetMotion.cs
+++ Assets/Scripts/barMagnetMotion.cs
@@ -11,6 +11,6 @@ public class barMagnetMotion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0, 0, 5));
+        transform.Rotate(new Vector3(0, 0, 1));
 	}
 }
