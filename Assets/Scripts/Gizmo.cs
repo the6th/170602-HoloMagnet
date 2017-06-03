e61daf77dbf11457a4f17920e893fa22a3529e6f
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2017-06-03 17:15:32 +0900

    🚧コンパスを作る：座標を向かせる

--- Assets/Scripts/Gizmo.cs
+++ Assets/Scripts/Gizmo.cs
@@ -15,15 +15,20 @@ public class Gizmo : MonoBehaviour
     /* このオブジェクトを中心に回転する。Pivot。ここまで。 */
 
     GameObject northPole;
+    GameObject southPole;
 
     void Start()
     {
          northPole = GameObject.Find("North Pole");
+         southPole = GameObject.Find("South Pole");
     }
 
     private void Update()
     {
         //transform.Rotate(new Vector3(0, 0, 5));
-        transform.LookAt(northPole.transform);
+        //transform.LookAt(northPole.transform);
+        //Vector3 selfToNorthPole = northPole.transform.transform.position - transform.transform.position; 
+        //transform.LookAt(selfToNorthPole);
+        transform.LookAt(northPole.transform.transform.position);
     }
 }
